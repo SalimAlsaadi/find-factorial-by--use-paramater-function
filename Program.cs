59c30aeb763813d9d4727fda87af37d38c8d0012
SalimAlsaadi <al-saadis-s-s@hotmail.com>
2023-06-06 09:02:39 -0700

    how to find factorial by use paramaster function and then call this function to find any factorial

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace functions_with_parametars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("please enter number to find Factorial: ");
+
+            int realCalculate = calculatFac(Console.ReadLine());//here we give value for variable by using paramater function.
+
+            
+            Console.WriteLine("the factorial of this number is : "+realCalculate);
+
+            static int calculatFac(string num)   //if function declared by prototype, then must teturn a value as same prototype given.
+            {                                   //this function is used for find factorial of any number that user entered.
+
+
+                int integerNum = int.Parse(num);
+                int fact = 1; 
+                for(int i=integerNum; i>1; i--)
+                {
+                    fact = fact * i;
+
+                }
+                return fact;//return value because thuis function is delcared.
+            }
+
+        }
+    }
+}
